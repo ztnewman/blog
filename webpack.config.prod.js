ad00cf6ef72f8a1004134f66e35c442b3d13c3be
Micah Cowell <micahcowell@gmail.com>
2016-10-07 21:46:44 -0700

    webpack production

---
+++ webpack.config.prod.js
@@ -0,0 +1,30 @@
+var webpack = require('webpack');
+
+module.exports = {
+	entry: './src/app.js',
+	output: { path: __dirname, filename: 'bundle.min.js' },
+	module: {
+		loaders: [
+			{
+				test: /.js?$/,
+				loader: 'babel-loader',
+				exclude: /node_modules/,
+				query: {
+					presets: ['es2015', 'react']
+        		}
+			}
+  		]
+	},
+	plugins:[
+		new webpack.DefinePlugin({
+			'process.env':{
+				'NODE_ENV': JSON.stringify('production')
+			}
+		}),
+		new webpack.optimize.UglifyJsPlugin({
+			compress:{
+				warnings: true
+			}
+		})
+	]
+}
